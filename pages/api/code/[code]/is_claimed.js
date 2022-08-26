a7135ab8529333ee71a8dbf391c5e14274fb162a
Marcelo Paez Sequeira <paezao@gmail.com>
2022-08-26 10:25:05 -0300

    Contract interaction working

--- pages/api/code/[code]/is_claimed.js
+++ pages/api/code/[code]/is_claimed.js
@@ -6,18 +6,22 @@ const contract = new Contract();
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
+        const returnClaimed = (claimed) => {
+            return res.status(200).json({ claimed });
+        };
+
         const { code } = req.query;
 
         const foundCode = await repository.isValid(code);
         if (!foundCode) {
-            return res.status(404).end();
+            return returnClaimed(true);
         }
 
         const claimed = await contract.isClaimed(code);
         if (claimed) {
-            return res.status(404).end();
+            return returnClaimed(true);
         }
 
-        res.status(200).end();
+        return returnClaimed(false);
     }
 }
